079e7a1067d42113d7e87081c0eff0c926e6cbcd
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2023-10-07 09:36:27 -0600

    Implements self.join relationship example

---
+++ db/migrate/20231007151816_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.references :manager, foreign_key: { to_table: :users }
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_06_220256) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_07_151816) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,4 +37,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_06_220256) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.bigint "manager_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["manager_id"], name: "index_users_on_manager_id"
+  end
+
+  add_foreign_key "users", "users", column: "manager_id"
 end
